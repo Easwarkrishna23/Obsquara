55a58908b8cdcc888413611aaa54049e387ac55b
Easwarkrishna23 <easwarkrishna23@gmail.com>
2024-04-30 13:58:20 +0530

    Exception examples commit

---
+++ EaswarJava/src/exception/Exception1.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class Exception1 {
+
+	public static void main(String[] args) {
+		// 
+		int a = 10;
+		int b = 0;
+		int c = a / b;
+		System.out.println(c);
+		System.out.println("This is Exception Handling example 1");
+
+	}
+
+}
---
+++ EaswarJava/src/exception/Exception2.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class Exception2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = null;
+		System.out.println(a.length());
+		
+
+	}
+
+}

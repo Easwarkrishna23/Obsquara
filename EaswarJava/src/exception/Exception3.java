8bfa622b42a28c72cdb9a11bffb2f5a621ed82be
Easwarkrishna23 <easwarkrishna23@gmail.com>
2024-05-02 10:46:31 +0530

    Exception 3 and 4 examples Commit

---
+++ EaswarJava/src/exception/Exception3.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class Exception3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = Integer.parseInt(null);
+
+	}
+
+}
---
+++ EaswarJava/src/exception/Exception4.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class Exception4 {
+
+	public static void main(String[] args) {
+		// Array index out of bound Exception
+		
+		int arr[] = {11,12,13,14};
+		int len = arr.length;
+		System.out.println(arr[len]);
+
+}
+}
